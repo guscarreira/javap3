7a58b4b5af7becc67f3c4cb1cf469799516897e3
Gustavo Carreira <126680947+guscarreira@users.noreply.github.com>
2024-02-27 21:20:00 -0300

    Add files via upload

---
+++ BABIGOL/src/main/java/br/com/fujideia/iesp/tecback/controller/HealthController.java
@@ -0,0 +1,19 @@
+package br.com.fujideia.iesp.tecback.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/health")
+public class HealthController {
+    @GetMapping
+    public String health(){
+        return "ok ok";
+    }
+    @PostMapping
+    public String teste(){
+        return "MUCANAL";
+    }
+}
